4dd2c5436ad40b1950bf9b4e0ee7f7a3b3447f27
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-07-27 20:31:56 -0700

    Updates

---
+++ precache-manifest.e6f25abc8016ab623cf32fc3ccc3cfbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d14c8769f0c2d2eb4954b807511f13",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "ba2a325fd999a4f3b387",
+    "url": "/clicky_game/static/css/main.681b3725.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky_game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "ba2a325fd999a4f3b387",
+    "url": "/clicky_game/static/js/main.a38971ae.chunk.js"
+  },
+  {
+    "revision": "ef6ad64b0df3315f3b6a",
+    "url": "/clicky_game/static/js/runtime~main.33627756.js"
+  }
+]);
